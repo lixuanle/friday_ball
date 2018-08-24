b70e0ae0a29f896abddfe93ecd23b3dd83fc134f
Isaac <isaacli911@gmail.com>
2018-08-24 09:24:32 -0400

    new update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const botconfig = require("./botconfig.json");
@@ -29,9 +30,9 @@ bot.on("message", async message => {
   let cmd = messageArray[0].toLowerCase();
   let args = messageArray.slice(1);
 
-  message.channel.send("<@" + message.author.id + ">");
+  // message.channel.send("<@" + message.author.id + ">");
 
-
+  // Adds users to the Members role
   if (message.channel.id == "482296848120545280") {
     message.delete();
     if (cmd === prefix + "role") {
@@ -47,32 +48,56 @@ bot.on("message", async message => {
     }
   }
 
+  // Event handler to handle Members leaving attendance and to handle tentative members to attend
   if (message.channel.id == "482296860556394496") {
     message.delete();
     if (cmd === prefix + "attending") {
-      let index = attendance_list.indexOf(message.author.id);
-      if(index === -1) {
-        attendance_list.push(message.author.id);
+      if (attendance_list.length === 12) {
+        // message.channel.send({embed: {
+        //   color: KEYAKI_GREEN,
+        //   description: "Attendance is maxed out, you can come if you'd like but your playing time might/will be limited."
+        // }})
+      } else {
+        let index = attendance_list.indexOf(message.author.id);
+        if(index === -1) {
+          attendance_list.push(message.author.id);
+          updateAttendance();
+        }
       }
     } else if (cmd === prefix + "notattending") {
       let index = attendance_list.indexOf(message.author.id);
       if(index !== -1) {
         attendance_list.splice(index, 1);
+        updateAttendance();
       }
     }
   }
 
 });
 
+// Updates attendance list(usually once per hour, but will also update when attendance list is updated).
 async function updateAttendance() {
-  // message.channel.bulkDelete(1);
-  // let embed = new Discord.RichEmbed()
-  //   .setAuthor("Weekly Attendance")
-  //   .setDescription()
-  //   .setColor(KEYAKI_GREEN)
-  //   .setFooter("Updated by the hour.")
-  //   .setTimestamp()
-  // bot.channels.get('482237205281112074').send({embed});
+  bot.channels.get('482296860556394496').bulkDelete(50);
+  let embed = new Discord.RichEmbed()
+    .setAuthor("Weekly Attendance")
+    .setDescription(
+      "\n1. " + returnString(attendance_list[0]) + "2. " + returnString(attendance_list[1]) + "3. " + returnString(attendance_list[2]) + "4. " + returnString(attendance_list[3])
+       + "5. " + returnString(attendance_list[4]) + "6. " + returnString(attendance_list[5]) + "7. " + returnString(attendance_list[6]) + "8. " + returnString(attendance_list[7])
+       + "9. " + returnString(attendance_list[8]) + "10. " + returnString(attendance_list[9]) + "11. " + returnString(attendance_list[10]) + "12. " + returnString(attendance_list[11])
+    )
+    .setColor(KEYAKI_GREEN)
+    .setFooter("Updated by the hour.")
+    .setTimestamp()
+  bot.channels.get('482296860556394496').send({embed});
+}
+
+// Function to return a proper string or a blank string depending on if a member is available.
+function returnString(member) {
+  if (member) {
+    return ("<@" + member + ">\n")
+  } else {
+    return "\n"
+  }
 }
 
 bot.login(process.env.DISCORD_TOKEN);
