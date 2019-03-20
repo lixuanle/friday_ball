ee45e57443895cea35909b7f7d8b6edea015841e
Isaac <isaacli911@gmail.com>
2019-03-20 11:48:10 -0400

    pushing latest version

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-require('dotenv').config();
+require('dotenv').config({path: '/home/friday_ball/.env'});
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const botconfig = require("./botconfig.json");
 const KEYAKI_GREEN = 0xa0d468;
-const accountSid = "AC5abd02de79b9c1bec599caba877ca6e6";
-const authToken = '9203da7f2210ee67aea969be167cbf0c';
-// const twilClient = require('twilio')(accountSid, authToken);
 attendance_list = [];
 
 bot.on("ready", function() {
@@ -14,9 +11,19 @@ bot.on("ready", function() {
   bot.user.setActivity("Monta Ellis Highlights", {type: 'WATCHING'});
   let member_list = bot.guilds.get('482296816180789268').roles.get('482296955935129601').members.map(m => m.id)
   for(i=0;i<member_list.length;i++) {
-    attendance_list.push(member_list[i]);
+    let index = attendance_list.indexOf(member_list[i]);
+    if(index === -1) {
+      attendance_list.push(member_list[i]);
+    }
   }
-  var timer = setInterval(updateAttendance, 3600000);
+  updateAttendance();
+  // var timer = setInterval(updateAttendance, 3600000);
+});
+
+bot.on("error", function() {
+  console.error;
+  console.log("goodbye");
+  bot.destroy();
 });
 
 bot.on("guildMemberAdd", (member) => {
@@ -54,13 +61,17 @@ bot.on("message", async message => {
 
   // Event handler to handle Members leaving attendance and to handle tentative members to attend
   if (message.channel.id == "482296860556394496") {
+    bot.channels.get('503562334032232468').send({embed: {
+      color: KEYAKI_GREEN,
+      description: message.author + " said: " + message
+    }})
     message.delete();
     if (cmd === prefix + "attending") {
-      if (attendance_list.length === 12) {
-        // message.channel.send({embed: {
-        //   color: KEYAKI_GREEN,
-        //   description: "Attendance is maxed out, you can come if you'd like but your playing time might/will be limited."
-        // }})
+      if (attendance_list.length === 15) {
+        message.author.send({embed: {
+          color: KEYAKI_GREEN,
+          description: "Attendance is maxed out, you can come if you'd like but your playing time might/will be limited."
+        }})
       } else {
         let index = attendance_list.indexOf(message.author.id);
         if(index === -1) {
@@ -74,13 +85,39 @@ bot.on("message", async message => {
         attendance_list.splice(index, 1);
         updateAttendance();
       }
+    } else if (cmd === prefix + "guest") {
+      let today = new Date();
+      let dd = today.getDay();
+      if (dd === 4 || dd === 5) {
+        let guest = args;
+        guest = guest.join(' ');
+        let index = attendance_list.indexOf(guest);
+        if (index === -1) {
+          attendance_list.push(guest);
+          updateAttendance();
+        }
+      }
+      else {
+        message.author.send({embed: {
+          color: KEYAKI_GREEN,
+          description: "Try inviting guests again on a Thursday or Friday."
+        }})
+      }
+    } else if (cmd === prefix + "remove") {
+      let guest = args;
+      guest = guest.join(' ');
+      let index = attendance_list.indexOf(guest);
+      if (index !== -1) {
+        attendance_list.splice(index, 1);
+        updateAttendance();
+      }
     }
   }
 
 });
 
 // Updates attendance list(usually once per hour, but will also update when attendance list is updated).
-async function updateAttendance() {
+function updateAttendance() {
   bot.channels.get('482296860556394496').bulkDelete(50);
   let embed = new Discord.RichEmbed()
     .setAuthor("Weekly Attendance")
@@ -88,19 +125,30 @@ async function updateAttendance() {
       "\n1. " + returnString(attendance_list[0]) + "2. " + returnString(attendance_list[1]) + "3. " + returnString(attendance_list[2]) + "4. " + returnString(attendance_list[3])
        + "5. " + returnString(attendance_list[4]) + "6. " + returnString(attendance_list[5]) + "7. " + returnString(attendance_list[6]) + "8. " + returnString(attendance_list[7])
        + "9. " + returnString(attendance_list[8]) + "10. " + returnString(attendance_list[9]) + "11. " + returnString(attendance_list[10]) + "12. " + returnString(attendance_list[11])
+       + "13. " + returnString(attendance_list[12]) + "14. " + returnString(attendance_list[13]) + "15. " + returnString(attendance_list[14])
     )
     .setColor(KEYAKI_GREEN)
-    .setFooter("Updated by the hour.")
+    .setFooter("Up to date as of: ")
     .setTimestamp()
   bot.channels.get('482296860556394496').send({embed});
+  return bot.channels.get('482296860556394496').send({embed: {
+    color: KEYAKI_GREEN,
+    description: "**HOW TO USE: ** \n`>attending` to join.\n `>notattending` to remove from list.\n `>guest` to bring a guest. \n `>remove` to remove guest."
+  }});
 }
 
 // Function to return a proper string or a blank string depending on if a member is available.
 function returnString(member) {
   if (member) {
-    return ("<@" + member + ">\n")
+    let reg = /^\d+$/;
+    if (reg.test(member)) {
+      let nickname = bot.guilds.get('482296816180789268').members.get(member).nickname;
+      return ("**" + nickname + "**\n");
+    } else {
+      return ("**" + member + " - Guest**\n");
+    }
   } else {
-    return "\n"
+    return "\n";
   }
 }
 
